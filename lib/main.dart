b2e94cc994ad9fa4bea89f084546a0107a0841f0
Jacobia <johnsonjacobia@gmail.com>
2020-05-15 02:03:15 -0400

    added missing MaterialApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('CHART!'),
-              elevation: 5,
-            ),
+    return MaterialApp(
+        title: 'FlutterApp',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Expense App'),
           ),
-          Card(
-            child: Text('LIST OF TX'),
+          body: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('CHART!'),
+                  elevation: 5,
+                ),
+              ),
+              Card(
+                child: Text('LIST OF TX'),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
